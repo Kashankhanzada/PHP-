8bca663a804e61d5fd83892a910b709af95ffd2e
Kashan Ali Khan <154746950+Kashankhanzada@users.noreply.github.com>
2025-06-26 09:48:16 +0500

    Add files via upload

---
+++ chapter-01/hello.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Hello World PHP</title>
+<script type="text/javascript">
+function MM_popupMsg(msg) { //v1.0
+  alert(msg);
+}
+</script>
+</head>
+<body>
+<?
+print"ยินดีต้อนรับสู่โลกของ PHP ค่ะ";
+?>
+
+</body>
+</html>
